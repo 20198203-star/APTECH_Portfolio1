bf11e621fa2397169e453f8f1fd1e197aa1615ff
Mara Gadgad <20198203@s.ubaguio.edu>
2026-05-15 19:14:06 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {Routes, Route, HashRouter} from "react-router-dom"
 import NavBar from './pages/NavBar'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
@@ -10,16 +10,16 @@ import './App.css'
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
     <NavBar />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="#/" element={<Home />} />
+      <Route path="#/projects" element={<Projects />} />
+      <Route path="#/contact" element={<Contact />} />
+      <Route path="#/admin" element={<Admin />} />
+      <Route path="#/dashboard" element={<Dashboard />} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/APTECH_Portfolio1/'
+  base: '/APTECH_Portfolio/'
 })
